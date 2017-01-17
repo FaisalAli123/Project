e7b3b0d0ff0f5e0410c91fb78904a34c24b0e387
FaisalAli123 <u1457222@unimail.hud.ac.uk>
2017-01-17 18:28:51 +0000

    i addded another web page called games

--- resources/views/front/games.blade.php
+++ resources/views/front/games.blade.php
@@ -9,32 +9,32 @@
       </div>
       <div class="col-md-4 col-sm-6">
           <a href="{{route('game')}}">
-              <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+              <img class="img-responsive img-portfolio img-hover" src="http://media1.gameinformer.com/filestorage/CommunityServer.Components.SiteFiles/imagefeed/featured/electronic-arts/battlefield/battlefield1/GasMask610.jpg" alt="">
           </a>
       </div>
       <div class="col-md-4 col-sm-6">
           <a href="{{route('game')}}">
-              <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+              <img class="img-responsive img-portfolio img-hover" src="https://ubistatic9-a.akamaihd.net/ubicomstatic/en-US/global/media/wd-media-ss06-FULL-marcus-beautiful-lies_254769.jpg" alt="">
           </a>
       </div>
       <div class="col-md-4 col-sm-6">
           <a href="{{route('game')}}">
-              <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+              <img class="img-responsive img-portfolio img-hover" src="https://ubistatic9-a.akamaihd.net/ubicomstatic/en-US/global/media/FH-screenshot-05-FULL_255355.jpg" alt="">
           </a>
       </div>
       <div class="col-md-4 col-sm-6">
           <a href="{{route('game')}}">
-              <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+              <img class="img-responsive img-portfolio img-hover" src="http://cdn2.alphr.com/sites/alphr/files/styles/gallery_adv/public/2016/05/gt_sport_gran_turismo_sport_release_date_1.jpg?itok=--NWfDkt" alt="">
           </a>
       </div>
       <div class="col-md-4 col-sm-6">
           <a href="{{route('game')}}">
-              <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+              <img class="img-responsive img-portfolio img-hover" src="http://images2.mobilenapps.com/data/images/full/13801/spider-man-ps4-december-2017-release-date-not-happening-new-video-teases-in-game-footage.jpg" alt="">
           </a>
       </div>
       <div class="col-md-4 col-sm-6">
           <a href="{{route('game')}}">
-              <img class="img-responsive img-portfolio img-hover" src="http://placehold.it/700x450" alt="">
+              <img class="img-responsive img-portfolio img-hover" src="http://assets1.ignimgs.com/thumbs/userUploaded/2016/6/13/2704682829328e9c64a2bo-1465876092206_large.jpg" alt="">
           </a>
       </div>
   </div>
